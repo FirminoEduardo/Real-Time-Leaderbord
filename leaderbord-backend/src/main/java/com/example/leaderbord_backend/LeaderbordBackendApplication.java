e7c568b1b9a25e0c1d01b2d331a45632f70187b7
FirminoEduardo <dudu.lf@outlook.com>
2025-03-08 11:56:31 -0300

    Estrutura do Projeto

---
+++ leaderbord-backend/src/main/java/com/example/leaderbord_backend/LeaderbordBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaderbord_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderbordBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderbordBackendApplication.class, args);
+	}
+
+}
---
+++ leaderbord-backend/src/test/java/com/example/leaderbord_backend/LeaderbordBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaderbord_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderbordBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
