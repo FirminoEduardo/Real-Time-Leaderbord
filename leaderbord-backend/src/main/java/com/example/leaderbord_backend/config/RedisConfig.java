d7f9fd2588675fa1a88cf41dc7a07851e9885e14
FirminoEduardo <dudu.lf@outlook.com>
2025-03-08 12:20:24 -0300

    Configuracao do MongoDB e do Redis no application.properties. Criacao da classe de configuracao do Redis

---
+++ leaderbord-backend/src/main/java/com/example/leaderbord_backend/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.example.leaderbord_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(); //Criação da conexão com o banco de dados
+    }
+
+    // Permite armazenar e recuperar dados do Redis
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+    // Facilita operações de String no Redis
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        return new StringRedisTemplate(redisConnectionFactory);
+    }
+}
